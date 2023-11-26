404c4aa2e10efad1d53995917ded11e9ef06a06e
Quynh Nhu Dang <87718231+quynhnhudang@users.noreply.github.com>
2023-11-26 16:53:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 document.getElementById('pronunciationBtn').addEventListener('click', function () {
-    // Add logic to play the pronunciation audio
+    // Add logic to play the pronunciation audio (you might use an audio library)
     alert('Playing pronunciation...');
 });
 
 document.getElementById('quizBtn').addEventListener('click', function () {
     // Redirect to the quiz page or load the quiz dynamically
     alert('Redirecting to quiz...');
+});
+
+// Sample navigation logic
+document.getElementById('homeBtn').addEventListener('click', function () {
+    document.getElementById('lessonTitle').innerText = 'Lesson 1: Greetings';
+    // Add logic to load home content dynamically
+});
+
+document.getElementById('lessonsBtn').addEventListener('click', function () {
+    document.getElementById('lessonTitle').innerText = 'Lessons';
+    // Add logic to load lessons dynamically
+});
+
+document.getElementById('progressBtn').addEventListener('click', function () {
+    document.getElementById('lessonTitle').innerText = 'Progress';
+    // Add logic to load user progress dynamically
+});
+
+document.getElementById('logoutBtn').addEventListener('click', function () {
+    // Add logic to log the user out
+    alert('Logging out...');
 });
