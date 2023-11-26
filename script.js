98d43fbaa01a17b1c2b3bf5211db44a5f6952296
Quynh Nhu Dang <87718231+quynhnhudang@users.noreply.github.com>
2023-11-26 16:45:23 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('pronunciationBtn').addEventListener('click', function () {
+    // Add logic to play the pronunciation audio
+    alert('Playing pronunciation...');
+});
+
+document.getElementById('quizBtn').addEventListener('click', function () {
+    // Redirect to the quiz page or load the quiz dynamically
+    alert('Redirecting to quiz...');
+});
